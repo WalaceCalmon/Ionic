39980b441e8431797b7d1afc653145447768cc3b
Walace.Calmon <walace.calmon@gmail.com>
2022-06-13 04:59:20 -0300

    OK

--- Aula-01/capacitor.config.ts
+++ Aula-01/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: '01-my-app',
+  appName: 'Aula-01',
   webDir: 'www',
   bundledWebRuntime: false
 };
