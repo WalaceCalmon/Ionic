31d5f41d154c0002210073d6524fdb2bda48d5f3
Walace Dias Calmon <walace.calmon@gmail.com>
2022-06-15 12:31:21 -0300

    Ok

--- 02-compomentes/src/app/pages/inicio/inicio.page.ts
+++ 02-compomentes/src/app/pages/inicio/inicio.page.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente{
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-android',
+      name: 'Alert',
+      redirecTo: '/alert'
+    }
+  ];
 
   constructor() { }
 
